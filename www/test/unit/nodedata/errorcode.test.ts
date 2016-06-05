e5d711e682350110b391ca0dda792006b8025575
Johann Haaf <draggha@gmail.com>
2016-06-05 16:46:23 +0200

    - moved "dist" and "examples" folders to root level (they aren't source code) - deleted some obsolete unit tests - removed some obsolete config files and properties

--- www/test/unit/nodedata/errorcode.test.ts
+++ www/test/unit/nodedata/errorcode.test.ts
@@ -15,48 +15,10 @@ function test1 (t) {
   t.equal(nodeData.errorCode(), 0, '-> should return 0 if no error code was given')
 }
 
-function test2 (t) {
-  // Arrange
-  let treeData = {
-    title: 'Node 1',
-    isLeaf: false,
-    errorCode: ErrorCodes.NOT_FOUND,
-    children: []
-  }
-  let nodeData
-
-  // Act
-  nodeData = createNodeData(treeData)
-
-  // Assert
-  t.equal(nodeData.errorCode(), ErrorCodes.NOT_FOUND, '-> should return the given errorCode when one is given into the constructor')
-}
-
-function test3 (t) {
-  // Arrange
-  let treeData = {
-    id: '1',
-    title: 'Root Node 1',
-    isLeaf: false,
-    errorCode: 0,
-    children: []
-  }
-  let nodeData = createNodeData(treeData)
-  let expectedErrorCode = ErrorCodes.UNAUTHORIZED
-
-  // Act
-  nodeData.errorCode(expectedErrorCode)
-
-  // Assert
-  t.equal(nodeData.errorCode(), expectedErrorCode, '-> should change the errorCode to a new value when an argument is given')
-}
-
 export default function (test) {
   test('NodeData.errorCode()', function (t) {
-    t.plan(3)
+    t.plan(1)
 
     test1(t)
-    test2(t)
-    test3(t)
   })
 }
